8cf8c214d9f8a09e192978161304a6d956ef2b88
devMarilia <devMarilia.frontend@gmail.com>
2022-10-12 20:38:45 -0300

    ✨ feat: Component Home

---
+++ src/components/Banner/styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   background: red ;
+   width: 100%;
+   height: 40vh;
+   display: flex;
+   justify-content: center;
+`;
