a889c00f64ecd04bc6a891ecb1545a24ff6e2f4c
devMarilia <devMarilia.frontend@gmail.com>
2022-10-12 18:19:39 -0300

    ✨ feat: Config ContexAPI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { ContextProvider } from './AppContext/Contex';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
   </React.StrictMode>
 )
