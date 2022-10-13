6bf25cb10263566f31aed5b46d55730ce7ec6c29
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-10-13 18:19:17 -0300

    finished routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ContextProvider } from './AppContext/Contex';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ContextProvider } from "./AppContext/Contex";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
-)
+);
