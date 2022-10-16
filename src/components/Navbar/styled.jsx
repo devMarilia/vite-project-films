b177607b32b01d0b4c6973ce735e311505130b8f
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-10-16 13:17:08 -0300

    Adjusted CSS navbar and formatted

--- src/components/Navbar/styled.jsx
+++ src/components/Navbar/styled.jsx
@@ -1,18 +1,19 @@
 import styled from "styled-components";
 
 export const NavbarConteiner = styled.header`
-
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 0 auto;
-    height: 2.4rem;
+  margin: 0 15px 0px 0;
+  height: 2.4rem;
   background: var(--color-navbar);
+
   .logo {
     font-weight: 700;
     margin: 15px;
     color: var(--text-color-primary);
   }
+
   .nav-items a {
     color: var(--text-color-primary);
     font-size: 0.75rem;
@@ -24,12 +25,15 @@ export const NavbarConteiner = styled.header`
       opacity: 1;
     }
   }
+
   .nav-items a:hover {
     opacity: 1;
   }
+
   .nav-toggle {
     display: none;
   }
+
   @media (max-width: 700px) {
     border-bottom: 100%;
 
@@ -46,23 +50,26 @@ export const NavbarConteiner = styled.header`
       height: 100%;
       transform: translateX(-100%);
       transition: all 0.45s;
-      z-index: 1 ;
+      z-index: 1;
     }
+
     .nav-items a {
       align-items: center;
-    text-decoration: none;
-    font-size: 1rem;
-    font-weight: 700;
-    color: var(--text-color-primary) ;
-    opacity: .9;
-
+      text-decoration: none;
+      font-size: 1rem;
+      font-weight: 700;
+      color: var(--text-color-primary);
+      opacity: 0.9;
     }
+
     nav-items > a::before {
       background: transparent;
     }
+
     .nav-items.open {
       transform: translateX(0);
     }
+
     .nav-toggle {
       display: flex;
       width: 50px;
@@ -71,6 +78,7 @@ export const NavbarConteiner = styled.header`
       justify-content: center;
       cursor: pointer;
     }
+
     .nav-toggle > .bar {
       position: relative;
       width: 32px;
@@ -78,6 +86,7 @@ export const NavbarConteiner = styled.header`
       background: var(--text-color-secondary);
       transition: all 0.45s ease-in-out;
     }
+
     .nav-toggle > .bar::before,
     .nav-toggle > .bar::after {
       content: "";
@@ -87,23 +96,28 @@ export const NavbarConteiner = styled.header`
       border-radius: 2px;
       transition: all 0.45s ease-in-out;
     }
+
     .nav-toggle .bar::before {
       width: 25px;
       transform: translateY(-8px);
       right: 0;
     }
+
     .nav-toggle > .bar::after {
       width: 32px;
       transform: translateY(8px);
     }
+
     .nav-toggle.open > .bar {
       transform: translateX(-40px);
       background: transparent;
     }
+
     .nav-toggle.open > .bar::before {
       width: 32px;
       transform: rotate(45deg) translate(26px, -26px);
     }
+
     .nav-toggle.open > .bar::after {
       transform: rotate(-45deg) translate(26px, 26px);
     }
