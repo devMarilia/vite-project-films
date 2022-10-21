3cbb1be5ec6677fb74b62b253e04288c3871b374
devMarilia <devMarilia.frontend@gmail.com>
2022-10-21 13:33:07 -0300

    🌈 style: Position elements css footer

--- src/components/Footer/styled.jsx
+++ src/components/Footer/styled.jsx
@@ -1,47 +1,58 @@
 import styled from "styled-components";
 
 export const ContainerFooter = styled.div`
-  height: 10rem;
   width: 100%;
   display: flex;
-  flex-direction: column ;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   align-content: center;
   border-top: 1px solid var(--background-mobile-menu);
+
+  .icon-rede-social {
+    display: flex;
+
+    > a {
+      font-size: 1rem ;
+      color: var(--color-primary) ;
+    }
+
+    > ul {
+      margin-top: -1rem ;
+    }
+  
+  }
 `;
 export const ContentFooter = styled.div`
   height: 5rem;
-  width: 90%;
+  width: 60%;
   color: #fff;
   display: flex;
   justify-content: space-around;
   align-items: center;
   align-content: center;
 
-  > h2 {
-    font-size: 1rem;
-    font-weight: bold;
-    text-align: center;
-  }
+
+ 
+
   .list {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
     flex-wrap: wrap;
+  
   }
-   a {
+  a {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    align-items: start;
+    justify-content: start;
     width: 100%;
-    color: #fff;
+    color: var(--text-color-secondary);
     text-decoration: none;
-    &:hover {
-        text-decoration: underline ;
-    }
 
+    font-size: 0.5rem;
+    line-height: 1rem;
+
+    &:hover {
+      text-decoration: underline;
+    }
   }
 `;
