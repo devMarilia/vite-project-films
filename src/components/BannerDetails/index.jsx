c21d82255794de9ec0d37f596fb5d3b1d00b9fed
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-10-23 01:25:42 -0300

    Adjusted

--- src/components/BannerDetails/index.jsx
+++ src/components/BannerDetails/index.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { ContainerBannerDetails } from './styled'
+import React from "react";
+import { ContainerBannerDetails } from "./styled";
 
 const BannerDetils = () => {
   return (
     <ContainerBannerDetails>
-     <div className='content-datails'>
-      <div className='content-datails-items-image'>
-        <img className='image-details' src='https://media.news9live.com/h-upload/2022/09/13/556728-newproject-2022-09-13t154154371.webp'/>
-      </div>
-      <div className='content-datails-items'>
-      <div>
-          <h3>Titulo</h3>
-          <span>10/15/2022</span>
-          <article>teste teste teste </article>
+      <div className="content-datails">
+        <div className="content-datails-items-image">
+          <img
+            className="image-details"
+            src="https://media.news9live.com/h-upload/2022/09/13/556728-newproject-2022-09-13t154154371.webp"
+          />
         </div>
-        
+        <div className="content-datails-items">
+          <div>
+            <h3>Titulo</h3>
+            <span>10/15/2022</span>
+            <h3>Sinopse</h3>
+            <article>teste teste teste </article>
+          </div>
+        </div>
       </div>
-      
-     </div>
     </ContainerBannerDetails>
-  )
-}
+  );
+};
 
-export default BannerDetils
+export default BannerDetils;
--- src/components/BannerDetails/styled.jsx
+++ src/components/BannerDetails/styled.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ContainerBannerDetails = styled.div`
   width: 100%;
-  height: 70vh;
+  height: 84vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,8 +16,8 @@ export const ContainerBannerDetails = styled.div`
     .content-datails-items-image {
       display: block;
       min-width: 150px;
-      width: 150px;
-      height: 220px;
+      width: 275px;
+      height: 433px;
       border-radius: 2px;
       position: relative;
       top: 0;
@@ -34,8 +34,9 @@ export const ContainerBannerDetails = styled.div`
     .content-datails-items {
       padding: 0px 20px;
       display: flex;
+      margin-top: -16rem;
       align-items: center;
-      color: #FFF ;
+      color: #fff;
     }
   }
 `;
