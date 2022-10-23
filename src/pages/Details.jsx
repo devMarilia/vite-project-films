4ca3cd13a3e08e6c41d8f99713fc252575d56fb9
devMarilia <devMarilia.frontend@gmail.com>
2022-10-22 22:47:08 -0300

    🎉 init: Root details

---
+++ src/pages/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+   <div>
+     <div>Banner</div>
+    <div>Elencos</div>
+   </div>
+  )
+}
+
+export default Details
--- src/router.jsx
+++ src/router.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 // Pages
 import Home from "./pages/Home";
 import Comedy from "./pages/Comedy";
+import Details from "./pages/Details";
 
 function MyRoutes() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route path="/detalhes:id" element={<Details />} />
       <Route path="/comedia" element={<Comedy />} />
     </Routes>
   );
