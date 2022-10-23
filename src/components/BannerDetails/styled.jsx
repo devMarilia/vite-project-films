012ada9fbf46bce89e5b81af0c735ceaab81fc5b
devMarilia <devMarilia.frontend@gmail.com>
2022-10-23 18:47:48 -0300

    🌈 style: ajusted styled banner details

--- src/components/BannerDetails/styled.jsx
+++ src/components/BannerDetails/styled.jsx
@@ -16,8 +16,6 @@ export const ContainerBannerDetails = styled.div`
     .content-datails-items-image {
       display: block;
       min-width: 150px;
-      width: 275px;
-      height: 433px;
       border-radius: 2px;
       position: relative;
       top: 0;
@@ -34,7 +32,6 @@ export const ContainerBannerDetails = styled.div`
     .content-datails-items {
       padding: 0px 20px;
       display: flex;
-      margin-top: -16rem;
       align-items: center;
       color: #fff;
     }
