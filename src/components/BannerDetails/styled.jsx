c485a604605d33b2dfc638f10db26ae5e47def60
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-10-24 17:57:59 -0300

    🦄 refactor:Adjustment

--- src/components/BannerDetails/styled.jsx
+++ src/components/BannerDetails/styled.jsx
@@ -4,7 +4,7 @@ export const ContainerBannerDetails = styled.div`
   width: 100%;
   height: 84vh;
   display: flex;
-  flex-direction: column ;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   border-bottom: 1px solid var(--background-mobile-menu);
@@ -30,35 +30,37 @@ export const ContainerBannerDetails = styled.div`
         object-fit: cover;
       }
     }
+
     .content-datails-items {
       padding: 0px 40px;
       display: flex;
       align-items: center;
       color: #fff;
 
-
       .items-datails {
         display: flex;
         flex-direction: column;
         justify-content: space-between;
 
-        > h3 {
-          font-size: 2rem ;
+        h3 {
+          font-size: 2rem;
+        }
+
+        sinopse {
         }
       }
     }
   }
+
   @media (max-width: 700px) {
     height: 142vh;
     width: 100%;
     justify-content: center;
     display: flex;
-    align-items: center ;
+    align-items: center;
 
-  
- 
-    .content-datails  {
-      flex-direction: column ;  
+    .content-datails {
+      flex-direction: column;
 
       .content-datails-items {
         padding: 0;
@@ -71,13 +73,11 @@ export const ContainerBannerDetails = styled.div`
       .content-datails-items-image {
         justify-content: center;
         display: flex;
-        align-items: center ;
+        align-items: center;
       }
     }
-  
   }
-  .content-datails-items .items-datails > h3 { 
-      font-size: 1rem ;
-    }
-   
+  .content-datails-items .items-datails > h3 {
+    font-size: 1rem;
+  }
 `;
