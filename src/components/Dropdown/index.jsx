dc7a2af6d1d37d21dc1367b437c5327c010aeb47
devMarilia <devMarilia.frontend@gmail.com>
2022-10-24 00:23:48 -0300

    ✨ feat: Dopdown menu complite

---
+++ src/components/Dropdown/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { MenuItems } from '../../services/menuItems';
+import { Link } from 'react-router-dom';
+import { ContainerDropdown } from './styled';
+
+function Dropdown() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <ContainerDropdown>
+      <ul
+        onClick={handleClick}
+        className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
+      >
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link
+                className={item.cName}
+                to={item.path}
+                onClick={() => setClick(false)}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </ContainerDropdown>
+  );
+}
+
+export default Dropdown;
--- src/router.jsx
+++ src/router.jsx
@@ -2,15 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
-import Comedy from "./pages/Comedy";
 import Details from "./pages/Details";
+import Genre from "./pages/Genre";
 
 function MyRoutes() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/detalhes/:id" element={<Details />} />
-      <Route path="/comedia" element={<Comedy />} />
+      <Route path="/genero" element={<Genre />} />
     </Routes>
   );
 }
