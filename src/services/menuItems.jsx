b458721bc2974bc38ab38010c22bc269f0533f90
devMarilia <devMarilia.frontend@gmail.com>
2022-10-23 21:13:28 -0300

    ✨ feat: Mock root navigate

---
+++ src/services/menuItems.jsx
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+      title: 'Novidades',
+      path: '/novidades',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Comédia',
+      path: '/comedia',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Documentários',
+      path: '/doumentarios',
+      cName: 'dropdown-link'
+    },
+  ];
